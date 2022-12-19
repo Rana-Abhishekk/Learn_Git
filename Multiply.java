6432bdac0b40d81619c30188ee12aafdf14ece87
Abhishek Singh Rana <abhishek17082001@gmail.com>
2022-12-19 11:13:40 +0530

    WE hava multiplication in multiply branch

--- Multiply.java
+++ Multiply.java
@@ -1,13 +1,13 @@
 
-public class Proj {
+public class Multiply {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a = 56;
 		int b = 64;
 		System.out.println("The sum of a and b is : " );
-		System.out.println(a+b);
-   
+		System.out.println(a*b);
+
 	}
 
 }
